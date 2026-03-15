7e5a4afdcd19e4fde4ec46bd6973828075dcf251
setor <marik123456789110011@gmail.com>
2026-03-15 23:32:45 +0300

    change sout UserService

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,7 +2,7 @@ package service;
 
 public class UserService {
     public static void main(String[] args) {
-        System.out.println("User Service");
+        System.out.println("User Service DEVELOP");
         System.out.println("User service commit 1");
     }
 }
