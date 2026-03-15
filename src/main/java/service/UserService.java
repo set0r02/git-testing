e343c4e480de7f15a181e4bf2972df2459d9ba35
setor <marik123456789110011@gmail.com>
2026-03-15 23:42:15 +0300

    update sout UserService

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -4,6 +4,6 @@ public class UserService {
     public static void main(String[] args) {
         System.out.println("User Service DEVELOP");
         System.out.println("User service commit 1");
-        System.out.println("User service commit 2");
+        System.out.println("User service commit 2 DEVELOP");
     }
 }
