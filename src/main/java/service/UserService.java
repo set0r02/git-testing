984fc162c7d4031581763019a3c3984c4f617d2e
setor <marik123456789110011@gmail.com>
2026-03-15 23:16:49 +0300

    Change User Service class

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -3,5 +3,6 @@ package service;
 public class UserService {
     public static void main(String[] args) {
         System.out.println("User Service");
+        System.out.println("User service commit 1");
     }
 }
